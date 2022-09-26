2ff3612cfa83a09e2901c76d5dbee10a1de418a7
AllisonPessoa <66957124+AllisonPessoa@users.noreply.github.com>
2022-09-25 22:00:40 -0300

    Changed setup to support multiple serial

--- examples/simple_command/simple_command.ino
+++ examples/simple_command/simple_command.ino
@@ -1,40 +1,52 @@
 #include <CommandHandler.h>
 
 int cmd_help();
-int cmd_led();
-int cmd_exit();
+int cmd_argument();
+int cmd_get_millis();
 
 CommandType Commands[] = {
-  {"help", &cmd_help, "Say Hi to the others"},
-  {"led", &cmd_led, "Turn on LED"},
-  {"exit", &cmd_exit, "Exit function"},
+  {"help", &cmd_help, "See the functions and corresponding descriptions"},
+  {"arg", &cmd_argument, "Print the first passed argument"},
+  {"millis", &cmd_get_millis, "Return the millis"},
 };
 
 CLI command_line(Commands, sizeof(Commands)/sizeof(Commands[0]));
 
 void setup(){
-  command_line.begin(9600);
+  Serial.begin(9600);
+  command_line.begin(&Serial);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop(){
-  command_line.start_processing();
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);   
+  command_line.start_processing();   // The processor will check if there is new inputs in Serial, otherwise, pass; 
+                                     // WARNING: If you put too much delay on loop(), it will take more time to process the commands. 
+                                     // A better solution is to use interrupts and Timers/Counters.
+                                     
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(100);                        
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(100);   
 }
 
 int cmd_help(){
   Serial.println("You asked for help");
+  Serial.println("Defined Functions - Description");
+  for(int i=0; i<sizeof(Commands)/sizeof(Commands[0]); i++){
+    Serial.print("> ");
+    Serial.print(Commands[i].comm_name);
+    Serial.print(" - ");
+    Serial.print(Commands[i].description);
+    Serial.println("");
+  }
 }
 
-int cmd_led(){
-  Serial.println("You asked to turn the LED on");
+int cmd_argument(){
+  Serial.println("You asked to see an argument");
   Serial.print("The argument is ");
   Serial.println(command_line.args[1]);
 }
 
-int cmd_exit(){
-  Serial.println("You asked to exit");
+int cmd_get_millis(){
+  Serial.println(millis());
 }
